dcb5e2f234306a5f3e4df43548edf44cafc6fb7f
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-21 22:05:58 -0400

    No license choice

--- index.js
+++ index.js
@@ -296,3 +296,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.`;
   }
+
+  // If user doesn't want to add the license
+  let showLicense = answers.license !== "None";
+
+  if (answers.license === "None") {
+    answers.license = "";
+  }
+
+  return `# ${answers.projectTitle}${badge}
