99c3a9699462d979689d06cf54c166653f2b372b
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-21 21:31:19 -0400

    Added MIT License

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function promptUser() {
       type: "input",
       name: "github",
       message:
-        "Please enter your GitHub username and repository URL for this project:",
+        "Please enter your GitHub username and repo URL for this project:",
     },
   ]);
 }
@@ -85,3 +85,28 @@ function generateREADME(answers) {
     if (answers.screenshots === "<skip>") {
         answers.screenshots = "";
     }
+
+    if (answers.license === "MIT") {
+      badge =
+          "[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)";
+      answers.license = `MIT License
+Copyright (c) ${answers.year} -- ${answers.name}
+  
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+  
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+  
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
+  }
